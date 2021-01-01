291f23896f4260ea70be2b432e0a7d47af4d8505
Haoming Yin <haomingyin@users.noreply.github.com>
2021-01-01 20:17:55 +1300

    Add favicons

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,13 +13,14 @@ DEFAULT_LANG = 'en'
 
 # Theme config
 THEME = 'themes/brutalist'
+FIRST_NAME = 'Haoming'
 SITEDESCRIPTION = 'A place where I note down my ideas.'
-FAVICON = 'profile.png'
+FAVICON = 'favicon'
 LOGO = 'profile.png'
-FIRSTNAME = 'Haoming'
-MENUITEMS = [('tags', '/tags')]
+MENUITEMS = [('Tags', '/tags')]
 GITHUB = 'https://github.com/haomingyin'
 DISQUS_SITENAME = 'haomingyin'
+FAVI_CON = "favicon"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -29,14 +30,14 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Pelican', 'https://getpelican.com/'),
+#          ('Python.org', 'https://www.python.org/'),
+#          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
